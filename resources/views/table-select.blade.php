3c85689ca437c06ef3e67b1d5e6d99cfbcedc5cb
dvarilek <davan@outlook.cz>
2025-06-08 19:41:03 +0200

    fix: undesired behaviour when form field is in modal and refactor view

--- resources/views/table-select.blade.php
+++ resources/views/table-select.blade.php
@@ -23,33 +23,33 @@
 @endphp
 
 <x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
-    :inline-label-vertical-alignment="\Filament\Support\Enums\VerticalAlignment::Center"
+        :component="$getFieldWrapperView()"
+        :field="$field"
+        :inline-label-vertical-alignment="\Filament\Support\Enums\VerticalAlignment::Center"
 >
     <x-filament::input.wrapper
-        :disabled="$isDisabled"
-        :inline-prefix="$isPrefixInline()"
-        :inline-suffix="$isSuffixInline()"
-        :prefix="$getPrefixLabel()"
-        :prefix-actions="$getPrefixActions()"
-        :prefix-icon="$getPrefixIcon()"
-        :prefix-icon-color="$getPrefixIconColor()"
-        :suffix="$getSuffixLabel()"
-        :suffix-actions="$getSuffixActions()"
-        :suffix-icon="$getSuffixIcon()"
-        :suffix-icon-color="$getSuffixIconColor()"
-        :valid="! $errors->has($statePath)"
-        :attributes="
+            :disabled="$isDisabled"
+            :inline-prefix="$isPrefixInline()"
+            :inline-suffix="$isSuffixInline()"
+            :prefix="$getPrefixLabel()"
+            :prefix-actions="$getPrefixActions()"
+            :prefix-icon="$getPrefixIcon()"
+            :prefix-icon-color="$getPrefixIconColor()"
+            :suffix="$getSuffixLabel()"
+            :suffix-actions="$getSuffixActions()"
+            :suffix-icon="$getSuffixIcon()"
+            :suffix-icon-color="$getSuffixIconColor()"
+            :valid="! $errors->has($statePath)"
+            :attributes="
             \Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())
                 ->class(['py-1.5', 'px-3', 'relative', 'min-h-9'])
         "
     >
         @if ($triggerSelectionActionOnInputClick)
             <x-filament::input
-                :autofocus="$isAutofocused()"
-                :id="$id"
-                :attributes="
+                    :autofocus="$isAutofocused()"
+                    :id="$id"
+                    :attributes="
                     (new \Illuminate\View\ComponentAttributeBag)
                         ->class(['absolute', 'inset-0', 'opacity-0', 'z-5', 'cursor-pointer', 'min-h-9'])
                         ->merge($selectionAction->isDisabled() ? [] : [
@@ -60,99 +60,104 @@
         @endif
 
         <div
-            x-data="{
+                x-data="{
                 state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }},
+                isMultiple: @js($isMultiple),
 
-                deleteOption: function (optionToDelete = null) {
-                    if (optionToDelete === null) {
+                isOptionSelected: function (optionKey = null) {
+                    if (! this.isMultiple && optionKey === null) {
+                        return this.state !== null;
+                    }
+
+                    if (Array.isArray(this.state)) {
+                        return this.state.includes(optionKey);
+                    }
+
+                    return false;
+                },
+
+                hasSelectedOptions() {
+                    if (! this.isMultiple) {
+                        return this.state !== null;
+                    }
+
+                    return Array.isArray(this.state) && this.state.length > 0;
+                },
+
+                deselectOption: function (optionToDelete = null) {
+                    if (! this.isMultiple && optionToDelete === null) {
                         this.state = null;
-
-                        $refs[`table-select-badge-{{ $id }}-single`].remove();
 
                         return;
                     }
 
-                    this.state = this.state?.filter(key => key !== optionToDelete);
-
-                    {{-- Since the only way a new item can be added is through the selection table which refreshes the field, it can just simply be removed without the state being live --}}
-                    $refs[`table-select-badge-{{ $id }}-${optionToDelete}`].remove();
+                    if (Array.isArray(this.state)) {
+                        this.state = this.state.filter(key => key !== optionToDelete);
+                    }
                 },
             }"
-            {{
-                $attributes
-                    ->merge([
-                        'id' => $id
-                    ], escape: false)
-                    ->merge($getExtraAttributes(), escape: false)
-                    ->class([
-                        "grid gap-2" => $isMultiple,
-                        "flex items-start gap-x-3 leading-5" => $isDisabled
-                    ])
-            }}
+                {{
+                    $attributes
+                        ->merge([
+                            'id' => $id
+                        ], escape: false)
+                        ->merge($getExtraAttributes(), escape: false)
+                        ->class([
+                            "grid gap-2" => $isMultiple,
+                            "flex items-start gap-x-3 leading-5" => $isDisabled,
+                        ])
+                }}
         >
-            @if ($isMultiple)
-                @if (filled($optionLabels = $getOptionLabels()))
-                    <div class="flex w-full flex-wrap gap-1.5">
-                        <template x-if="state.length === 0">
-                            <div class="text-gray-400 dark:text-gray-500 h-6">
-                                {{ $placeholder }}
-                            </div>
-                        </template>
+            <div
+                    x-show="! hasSelectedOptions()"
+                    class="text-gray-400 dark:text-gray-500 w-full h-6"
+            >
+                {{ $placeholder }}
+            </div>
 
-                        @foreach ($optionLabels as $optionKey => $optionLabel)
-                            <x-filament::badge
-                                x-ref="table-select-badge-{{ $id }}-{{ $optionKey }}"
+            @if ($isMultiple && filled($optionLabels = $getOptionLabels()))
+                <div
+                        x-show="hasSelectedOptions()"
+                        class="flex w-full flex-wrap gap-1.5 py-0.5"
+                >
+                    @foreach ($optionLabels as $optionKey => $optionLabel)
+                        <x-filament::badge
+                                x-show="isOptionSelected('{{ $optionKey }}')"
                                 class="z-10"
                                 :size="$optionSize"
                                 :iconSize="$optionIconSize"
                                 :color="$getOptionColor($optionKey) ?? null"
                                 :icon="$getOptionIcon($optionKey) ?? null"
                                 :attributes="$optionExtraAttributes"
-                            >
-                                {{ $optionLabel }}
+                        >
+                            {{ $optionLabel }}
 
-                                @unless($isDisabled)
-                                    <x-slot
+                            @unless($isDisabled)
+                                <x-slot
                                         name="deleteButton"
-                                        x-on:click.stop="deleteOption('{{ $optionKey }}')"
-                                    ></x-slot>
-                                @endunless
-                            </x-filament::badge>
-                        @endforeach
-                    </div>
-                @else
-                    <div class="text-gray-400 dark:text-gray-500 h-6">
-                        {{ $placeholder }}
-                    </div>
-                @endif
+                                        x-on:click.stop="deselectOption('{{ $optionKey }}')"
+                                ></x-slot>
+                            @endunless
+                        </x-filament::badge>
+                    @endforeach
+                </div>
             @elseif (filled($optionLabel = $getOptionLabel()))
-                <div class="flex w-full items-center">
-                    <template x-if="state === null">
-                        <div class="text-gray-400 dark:text-gray-500 w-full h-6">
-                            {{ $placeholder }}
-                        </div>
-                    </template>
-
-                    <div
-                        x-ref="table-select-badge-{{ $id }}-single"
-                    >
+                <div
+                        x-show="hasSelectedOptions()"
+                        class="flex w-full items-center h-6"
+                >
+                    <div class="truncate">
                         {{ $optionLabel }}
                     </div>
 
                     @unless($isDisabled)
-                        <template x-if="state !== null">
-                            <x-filament::icon-button
-                                    icon="heroicon-o-x-mark"
-                                    color="gray"
-                                    class="ml-auto z-10"
-                                    x-on:click="deleteOption()"
-                            />
-                        </template>
+                        <x-filament::icon-button
+                                icon="heroicon-o-x-mark"
+                                color="gray"
+                                class="ml-auto z-10"
+                                x-on:mousedown.stop.prevent="deselectOption()"
+                        />
                     @endunless
-                </div>
-            @else
-                <div class="text-gray-400 dark:text-gray-500 h-6">
-                    {{ $placeholder }}
                 </div>
             @endif
         </div>
@@ -160,11 +165,11 @@
 
     @unless ($triggerSelectionActionOnInputClick)
         <div
-            @class([
-                'justify-self-start' => $selectionActionAlignment === Alignment::Start || $selectionActionAlignment === Alignment::Right,
-                'justify-self-center' => $selectionActionAlignment === Alignment::Center || $selectionActionAlignment === Alignment::Between,
-                'justify-self-end' => $selectionActionAlignment === Alignment::End || $selectionActionAlignment === Alignment::Left,
-            ])
+                @class([
+                    'justify-self-start' => $selectionActionAlignment === Alignment::Start || $selectionActionAlignment === Alignment::Right,
+                    'justify-self-center' => $selectionActionAlignment === Alignment::Center || $selectionActionAlignment === Alignment::Between,
+                    'justify-self-end' => $selectionActionAlignment === Alignment::End || $selectionActionAlignment === Alignment::Left,
+                ])
         >
             {{ $selectionAction }}
         </div>
