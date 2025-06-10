dvarilek/filament-table-select

commit 00316b38cb5394613b09b32a8ab6b660382d27d5

fix: fix null selection limit

src/Components/Form/Concerns/HasSelectionModalCreateOptionAction.php
@@ -127,18 +127,19 @@ trait HasSelectionModalCreateOptionAction
                 if ($component->evaluate($component->requiresSelectionConfirmation)) {
                     $statePath = Js::from($component->getStatePath());
                     $createdOptionKey = Js::from($createdOptionKey);
+                    $selectionLimit = Js::from($selectionLimit);
 
                     $component->getLivewire()->js(<<<JS
-                        if ({$selectionLimit} === 1) {
+                        if ($selectionLimit === 1) {
                             Alpine.store('selectionModalCache').set($statePath, [$createdOptionKey]);
-        
+
                             return;
                         }
-        
-                        if (Alpine.store('selectionModalCache').get($statePath)?.length >= $selectionLimit) {
+
+                        if ($selectionLimit !== null && Alpine.store('selectionModalCache').get($statePath)?.length >= $selectionLimit) {
                             return;
                         }
-        
+
                         Alpine.store('selectionModalCache').push($statePath, $createdOptionKey);
                     JS);
                 } elseif ($selectionLimit === 1 || $selectionLimit >= count($state)) {

resources/views/selection-table-handler.blade.php
@@ -27,7 +27,7 @@
             }
 
             {{-- Prevent bulk select checkboxes from breaking stuff --}}
-            if (records.length > selectionLimit) {
+            if (selectionLimit !== null && records.length > selectionLimit) {
                 suppressWatcherForNextCycle = true;
                 selectedRecords = [...cachedSelectedRecords];
 
@@ -65,12 +65,11 @@
         suppressWatcherForNextCycle: true,
 
         updateCheckboxSelectability(records) {
-            const checkboxes = $wire.$el.querySelectorAll('.fi-ta-record-checkbox');
-
-            if (selectionLimit === 1) {
+            if (selectionLimit === 1 || selectionLimit === null) {
                 return;
             }
 
+            const checkboxes = $wire.$el.querySelectorAll('.fi-ta-record-checkbox');
             const limitReached = records.length >= selectionLimit;
 
             checkboxes.forEach(checkbox => checkbox.disabled = !checkbox.checked && limitReached);

commit 94093df7f433adcb849aff0987e97d2853935439

fix: fix oversight introduced in 00316b3

src/Components/Form/Concerns/HasSelectionModalCreateOptionAction.php
@@ -142,7 +142,7 @@ trait HasSelectionModalCreateOptionAction
 
                         Alpine.store('selectionModalCache').push($statePath, $createdOptionKey);
                     JS);
-                } elseif ($selectionLimit === 1 || $selectionLimit >= count($state)) {
+                } elseif ($selectionLimit === 1 || $selectionLimit === null || $selectionLimit >= count($state)) {
                     $jsonState = Js::from(is_array($state) ? $state : [$state]);
                     $statePath = Js::from($component->getStatePath());
 