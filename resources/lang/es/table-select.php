b4ace71c930ef0af2c30c76fb56b8b392206a25a
Gustavo (tavcode) <elmergustavo79@gmail.com>
2025-07-20 01:28:44 -0600

    Add Spanish translation for Table Select

---
+++ resources/lang/es/table-select.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'placeholder' => 'Selecciona un registro',
+
+    'actions' => [
+
+        'selection' => [
+
+            'view-label' => '{1} Ver registro relacionado|[2,*] Ver registros relacionados',
+
+            'edit-label' => '{1} Seleccionar registro relacionado|[2,*] Seleccionar registros relacionados',
+
+        ],
+
+        'selection-confirmation' => [
+
+            'label' => 'Confirmar',
+        ],
+
+        'selection-create-option' => [
+
+            'label' => 'Crear'
+
+        ]
+
+    ]
+
+];
